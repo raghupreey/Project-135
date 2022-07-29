537a71e2a9a30b55b1ed4107a2683e113bd543b5
raghupreey <89856655+raghupreey@users.noreply.github.com>
2022-07-29 19:31:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+img = "";
+objects = [];
+status = "";
+
+
+
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
+function start(){
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+}
